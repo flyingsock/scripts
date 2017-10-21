003fbbbe83400737e098665dc24269c9b85601ee
flyingsock <voodoo.people.magic@gmail.com>
2017-10-21 23:22:18 +0500

    hashes.js works with files

--- hashes/hashes.js
+++ hashes/hashes.js
@@ -1,37 +1,49 @@
+const fs = require("fs");
 
-var hashes = new Vue({
-    el: "#hashes",
-    data: {             //блок данных
-        message: "",    //входящая строка
-        substring: "",  //входящая подстрока
-        indexes: []     //индекс вхождения подстроки в строку
+var HASHES = {
+    fromFile: function(fileName) {      //чтение из файла
+        return fs.readFileSync(fileName, "utf8");
     },
-    methods: {
-        search: function() {
-            this.indexes = [];
-            let substringSize = this.substring.length;
-            let subStrings = [];
-            /*обработчик неверных событий*/
-            if ((substringSize > this.message.length) || (substringSize === 0) || (this.message.length === 0)) {
-                alert("NO!");
-                return;
-            }
-            //в массив subStrings кладутся все подстроки из строки длиной в искомую
-            for (let i = 0; i < this.message.length; i++) {
-                subStrings.push(this.message.slice(i, i + substringSize));
-            }
-            //сравнение хешей подстроки с элементами из subStrings
-            for (let i = 0; i < subStrings.length; i++) {
-                if (hashFunction(subStrings[i]) === hashFunction(this.substring)) {
-                    this.indexes.push(i);
+
+    search: function(message, substring) {
+        indexes = [];
+        let substringSize = substring.length;
+
+                //сравнение хешей подстроки с элементами из subStrings
+        for (let i = 0; i < message.length; i++) {
+            let currentSubstring = message.slice(i, i + substringSize);
+            if (this.hashFunction(currentSubstring) === this.hashFunction(substring)) {
+                if (currentSubstring === substring) {
+                    indexes.push(i);
                 }
             }
         }
+        return indexes;
     },
-});
-function hashFunction(str) {            //хеш-функция
-    return str.split("").reduce((previous, current) => {
-        previous += (previous - current.charCodeAt(0));
-        return previous;
-    }, 0);
+
+    hashFunction: function(str) {            //хеш-функция
+        let letters = str.split("");
+        let result = 0;
+        for (let i=0; i < str.length; i++) {
+            result += str.charCodeAt(i) - i;
+        }
+        return result % 256;
+    }
+}
+
+const inputFile = process.argv[2];    //откуда читать
+const substring = process.argv[3];    //какую подстроку читать
+
+try {
+    var message = HASHES.fromFile(inputFile);
+
+    /*обработчик неверных событий*/
+    if ((substring.length > message.length) || (substring.length === 0) || (message.length === 0)) {
+        console.log("no!");
+    } else {
+        var indexes = HASHES.search(message, substring);
+        console.log(indexes);
+    }
+} catch (error) {
+    console.log("error");
 }
